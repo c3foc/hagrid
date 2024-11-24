fae176ead92c29e697a13f4fcac3b3fa72f1ae26
Paul Bienkowski <pb@opatut.de>
2024-11-24 12:09:23 +0100

    add price/position editing to products table

--- hagrid/products/admin.py
+++ hagrid/products/admin.py
@@ -4,14 +4,19 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(StoreSettings)
-admin.site.register(Product)
 admin.site.register(Variation)
 admin.site.register(SizeGroup)
 admin.site.register(VariationAvailabilityEvent)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'position']
+    list_editable = ['price', 'position']
 
 @admin.register(Size)
 class SizeAdmin(admin.ModelAdmin):
     list_display = ['name', 'group']
     list_display_links = ['group']
     list_filter = ['group']
+
 
