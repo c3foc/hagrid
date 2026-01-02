87d36cffa29a79bb1f7ca3c854d9dd06e8e00cde
Doralitze <doralitze@chaotikum.org>
2026-01-02 19:23:42 +0100

    add: csrf_exempt to static pages view in urls.py

--- hagrid/staticpages/urls.py
+++ hagrid/staticpages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
 
 from .views import StaticPageView
 
 urlpatterns = [
-    path('<slug:page_slug>/', StaticPageView.as_view(), name='staticpages'),
+    path('<slug:page_slug>/', csrf_exempt()(StaticPageView.as_view()), name='staticpages'),
 ]
