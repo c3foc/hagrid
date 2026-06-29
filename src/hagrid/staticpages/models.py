92fe2dd8286486d81e08c872819bc957b72d2d68
Felix Rindt <felix@rindt.me>
2026-06-29 16:03:01 +0200

    add favicon, remove StaticPageImage, fix StaticPage footer link optics

--- src/hagrid/staticpages/models.py
+++ src/hagrid/staticpages/models.py
@@ -18,19 +18,3 @@ class StaticPage(models.Model):
 
     def __str__(self):
         return self.title
-
-
-def image_upload_path(instance, filename):
-    return f"pageimages/{instance.page.slug}/{filename}"
-
-
-class PageImage(models.Model):
-    page = models.ForeignKey(StaticPage, on_delete=models.CASCADE, related_name="images")
-    image = models.ImageField(upload_to=image_upload_path)
-    caption = models.CharField(max_length=200, blank=True)
-    alt_text = models.TextField(blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return f"{self.page.title} - {self.image.filename}"
