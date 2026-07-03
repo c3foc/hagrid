898e329d5c645415cc3f41b9b882d265dbb9e603
Felix Rindt <felix@rindt.me>
2026-07-03 12:20:53 +0200

    remove prod settings

--- src/hagrid/__main__.py
+++ src/hagrid/__main__.py
@@ -6,7 +6,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hagrid.prod_settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hagrid.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
